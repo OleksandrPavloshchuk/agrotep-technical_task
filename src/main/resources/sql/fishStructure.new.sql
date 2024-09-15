a5ffb493333e334f24506a803315e858082ebc53
Aleksandr Pavloshchuk <a.pavloshchuk@yetlo.com>
2024-09-15 14:40:58 +0300

    mistake in table 'fish'

--- src/main/resources/sql/fishStructure.new.sql
+++ src/main/resources/sql/fishStructure.new.sql
@@ -3,7 +3,6 @@ CREATE TABLE `fish`
 (
     `id`              int    NOT NULL AUTO_INCREMENT,
     `catch_date`      datetime(6)  DEFAULT NULL,
-    `image_file_name` varchar(255) DEFAULT NULL,
     `name`            varchar(255) DEFAULT NULL,
     `price`           double NOT NULL,
     PRIMARY KEY (`id`)
